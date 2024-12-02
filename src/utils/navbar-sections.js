3263d8431985c0d9e00856a0cab945679cdc4c61
tomikorzu <tomykorzu@icloud.com>
2024-12-02 13:21:37 -0300

    feat(navbar-sections.js): add the links that the navbar will have

---
+++ src/utils/navbar-sections.js
@@ -0,0 +1,27 @@
+export const navbarSections = [
+    {
+        "icon": "fa-solid fa-home",
+        "title": "Home",
+        "url": "/"
+    },
+    {
+        "icon": "fa-solid fa-user",
+        "title": "About",
+        "url": "/about"
+    },
+    {
+        "icon": "fa-solid fa-bars-progress",
+        "title": "Projects",
+        "url": "/projects"
+    },
+    {
+        "icon": "fa-solid fa-tools",
+        "title": "Services",
+        "url": "/services"
+    },
+    {
+        "icon": "fa-solid fa-envelope",
+        "title": "Contact",
+        "url": "/contact"
+    }
+]
