0c0761d7ec9e766d9574fa20291bd51a4e1710f0
tomikorzu <tomykorzu@icloud.com>
2024-12-02 13:34:34 -0300

    feat(netlify): deploy this repo to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import vercel from '@astrojs/vercel/serverless';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: vercel()
+  adapter: netlify()
 });
