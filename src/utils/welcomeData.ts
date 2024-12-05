0aea77666ac72d43f15bfdc0a0b9a4337e1059db
tomikorzu <tomykorzu@icloud.com>
2024-12-05 01:35:49 -0300

    feat(welcomeData.ts): add the text data that is in the welcome page in this file to only have to change it if is needed

---
+++ src/utils/welcomeData.ts
@@ -0,0 +1,16 @@
+export const welcomeData: WelcomeDataTypes = {
+  image: "/me.jpeg",
+  actualWork: "GM2Dev",
+  workPage: "https://gm2dev.com",
+  title: "Hey, I am Tomás Korzusehec",
+  subtitle:
+    "Full-Stack Developer from Argentina 🇦🇷 with experience in React, Astro, NodeJS and other tecnologies",
+};
+
+type WelcomeDataTypes = {
+  image: string;
+  actualWork?: string;
+  workPage?: string;
+  title: string;
+  subtitle: string;
+};
