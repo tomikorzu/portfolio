e2b3df92cc5d3e85806db903ccb97c25f5ec2d48
tomikorzu <tomykorzu@icloud.com>
2024-12-06 17:05:55 -0300

    fix(SocialLinks): change the SocialLinks UI

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -4,27 +4,30 @@ export default function SocialLinks() {
       <ul className={`flex items-center justify-center gap-5 m-2.5`}>
         <li>
           <a
-            className={`text-lg dark:bg-slate-800 bg-slate-200 rounded-full flex justify-center items-center w-10 h-10 transition duration-500 hover:brightness-125`}
+            className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-150`}
             href="https://github.com/tomikorzu"
             target="_BLANK"
+            style={{ transition: "all 0.4s" }}
           >
             <i className="fa-brands fa-github"></i>
           </a>
         </li>
         <li>
           <a
-            className={`text-lg dark:bg-slate-800 bg-slate-200 rounded-full flex justify-center items-center w-10 h-10 transition duration-500 hover:brightness-125`}
+            className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-150`}
             href="https://www.linkedin.com/in/TomasKorzusehec"
             target="_BLANK"
+            style={{ transition: "all 0.5s" }}
           >
             <i className="fa-brands fa-linkedin"></i>
           </a>
         </li>
         <li>
           <a
-            className={`text-lg dark:bg-slate-800 bg-slate-200 rounded-full flex justify-center items-center w-10 h-10 transition duration-500 hover:brightness-125`}
+            className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-150`}
             href="https://calendly.com/tomykorzu/30min?month=2024-12"
             target="_BLANK"
+            style={{ transition: "all 0.5s" }}
           >
             <i className="fa-solid fa-calendar-days"></i>
           </a>
