f6c988dbc09c9ffc62c843f0733338518e5e27bf
tomikorzu <tomykorzu@icloud.com>
2024-12-23 03:04:41 -0300

    feat(Card): add this component to connect it in about page

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,28 @@
 import aboutData from "../../data/aboutData.json";
 
-export default function Card({ type }: { type: string }) {
-  const content = aboutData[type];
+export default function Card({
+  title,
+  name,
+  startDate,
+  endDate,
+  image,
+  description,
+}: {
+  title: string;
+  name: string;
+  startDate: string;
+  endDate: string;
+  image: string;
+  description: string;
+}) {
   return (
-    <div className="container">
-      <h2>{type}</h2>
-      <div className="card">
-        <img
-          src={content.image}
-          alt={`${content.title} image`}
-          className="w-16 h-16"
-        />
-      </div>
-    </div>
+    <li className="w-full h-[200px] bg-slate-400 rounded-xl relative">
+      <img
+        src={image}
+        alt={`${title} image`}
+        className="w-10 h-10 object-cover absolute right-2 top-2 rounded-full"
+      />
+      <h2 className="max-w-[250px] mt-2">{title}</h2>
+    </li>
   );
 }
