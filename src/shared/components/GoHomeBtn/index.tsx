8ace868d9dcd7e1a8c3b69bb4b0fd8ebd5305ee0
tomikorzu <tomykorzu@icloud.com>
2024-12-27 02:50:35 -0300

    feat(GoHomeBtn): add this component layout

---
+++ src/shared/components/GoHomeBtn/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export default function GoHomeBtn() {
+  const [showBtn, setShowBtn] = useState(false);
+  function appearBtn() {
+    if (window.scrollY >= 100) {
+      setShowBtn(true);
+    } else {
+      setShowBtn(false);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", appearBtn);
+
+    return () => {
+      window.removeEventListener("scroll", appearBtn);
+    };
+  }, []);
+
+  function goHome() {
+    console.log("a");
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
+  return (
+    <button
+      className={`${
+        !showBtn ? "opacity-[100%]" : "opacity-0"
+      } fixed bottom-5 right-5 p-4 transition duration-500 bg-[#333] rounded-full flex z-20 text-xl`}
+      onClick={goHome}
+    >
+      <i className="fa-solid fa-arrow-up"></i>
+    </button>
+  );
+}
