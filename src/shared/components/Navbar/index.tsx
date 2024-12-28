58e9f2dad9cc08acfaf9ef0164d074ab625af380
tomikorzu <tomykorzu@icloud.com>
2024-12-28 05:24:09 -0300

    feat(Navbar): add toggle button

--- src/shared/components/Navbar/index.tsx
+++ src/shared/components/Navbar/index.tsx
@@ -3,8 +3,9 @@ import Link from "./Link";
 
 const sections = ["projects", "journey", "skillset", "about", "contact"];
 
-export default function ScrollNavbar() {
+export default function Navbar() {
   const [bgClass, setBgClass] = useState("bg-[transparent]");
+  const [isMenuActive, setIsMenuActive] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,15 +20,41 @@ export default function ScrollNavbar() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  function handleToggleBtn() {
+    setIsMenuActive(!isMenuActive);
+  }
+
   return (
-    <nav className="fixed top-2 z-50 w-full flex justify-center items-center">
-      <ul
-        className={`flex ${bgClass} rounded-full px-2 py-1 w-fit transition duration-[.4s]`}
+    <>
+      <nav className="fixed top-2 z-50 w-full hidden sm:flex justify-center items-center">
+        <ul
+          className={`flex ${bgClass} rounded-full px-2 py-1 w-fit transition duration-[.4s]`}
+        >
+          {sections.map((section) => {
+            return <Link key={section} name={section} />;
+          })}
+        </ul>
+      </nav>
+      <button
+        type="button"
+        onClick={handleToggleBtn}
+        className={`sm:hidden fixed flex flex-col top-4 right-5 z-20  w-11 h-11 justify-around p-2.5 rounded-full ${
+          isMenuActive ? "bg-transparent" : "bg-[#642164]"
+        } transition duration-[.4s]`}
       >
-        {sections.map((section) => {
-          return <Link key={section} name={section} />;
-        })}
-      </ul>
-    </nav>
+        <span
+          className={`w-full h-1 rounded-3xl transition-all duration-[0.4s] ${
+            isMenuActive ? "rotate-45 origin-top-left bg-white" : "bg-[#e2e2e2]"
+          }`}
+        ></span>
+        <span
+          className={`w-full h-1 rounded-3xl transition-all duration-[0.4s] ${
+            isMenuActive
+              ? "origin-bottom-left -rotate-45 translate-x-[0px] translate-y-[4px] bg-white"
+              : "w-[70%] bg-[#e2e2e2]"
+          }`}
+        ></span>
+      </button>
+    </>
   );
 }
