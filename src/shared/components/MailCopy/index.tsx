3158b771e181d7511833f3c68d3aa635a4fa3c50
tomikorzu <tomykorzu@icloud.com>
2024-12-31 05:00:45 -0300

    feat(MailCopy): add this component to copy the email

---
+++ src/shared/components/MailCopy/index.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function MailCopy() {
+  const [isCopied, setIsCopied] = useState(false);
+  const email = "tomykorzu@icloud.com";
+  function copyEmail() {
+    navigator.clipboard.writeText(email);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1500);
+  }
+  return (
+    <div className="flex items-center mt-3 mb-1">
+      <input
+        type="text"
+        readOnly
+        placeholder={email}
+        className="bg-transparent rounded-lg border-[1px] border-solid border-[#ffffff60] rounded-r-none  placeholder:text-slate-50 font-semibold text-md p-2 outline-none"
+      />
+      <button
+        type="button"
+        onClick={copyEmail}
+        className="p-2 px-3 border-[1px] border-solid text-white border-[#ffffff60] rounded-lg border-l-0 rounded-l-none"
+      >
+        <i
+          className={`fa-${isCopied ? "solid fa-check" : "regular fa-copy"}`}
+        ></i>
+      </button>
+    </div>
+  );
+}
