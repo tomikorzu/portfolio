b4bc2dea9558e5a56a8362a5b3e4fc85fd01e741
tomikorzu <tomykorzu@icloud.com>
2025-01-27 04:36:02 -0300

    feat(MailCoppy): change the width of the input only at the moment

--- src/shared/components/MailCopy/index.tsx
+++ src/shared/components/MailCopy/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import data from "../../data/welcomeData.json"
 
 export default function MailCopy() {
   const [isCopied, setIsCopied] = useState(false);
-  const email = "tomykorzu@icloud.com";
+  const {socials} = data
   function copyEmail() {
-    navigator.clipboard.writeText(email);
+    navigator.clipboard.writeText(socials.email);
     setIsCopied(true);
     setTimeout(() => {
       setIsCopied(false);
@@ -15,13 +16,13 @@ export default function MailCopy() {
       <input
         type="text"
         readOnly
-        placeholder={email}
-        className="bg-transparent rounded-lg border-[1px] border-solid border-[#ffffff60] rounded-r-none  placeholder:text-slate-50 font-semibold text-sm p-2 outline-none"
+        value={socials.email}
+        className="bg-transparent rounded-lg border-[1px] border-solid border-[#ffffff60] rounded-r-none  placeholder:text-slate-50 font-semibold text-sm p-2 outline-none w-[220px]"
       />
       <button
         type="button"
         onClick={copyEmail}
-        className="p-2 px-3 text-sm border-[1px] border-solid text-white border-[#ffffff60] rounded-lg border-l-0 rounded-l-none"
+        className="py-2 px-3 text-sm border-[1px] border-solid text-white border-[#ffffff60] rounded-lg border-l-0 rounded-l-none"
       >
         <i
           className={`fa-${isCopied ? "solid fa-check" : "regular fa-copy"}`}
