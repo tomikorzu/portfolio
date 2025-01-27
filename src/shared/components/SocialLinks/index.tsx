61fccfbdf92ac134c77b632d269162ae0e7df66f
tomikorzu <tomykorzu@icloud.com>
2025-01-27 04:55:58 -0300

    feat(CV): add my CV as socialLink

--- src/shared/components/SocialLinks/index.tsx
+++ src/shared/components/SocialLinks/index.tsx
@@ -1,7 +1,9 @@
-import data from "../../data/welcomeData.json"
+import data from "../../data/welcomeData.json";
 
 export default function SocialLinks() {
-    const {socials: {github, linkedin, calendly}} = data
+  const {
+    socials: { github, linkedin, calendly, CV },
+  } = data;
   return (
     <>
       <ul className={`flex items-center sm:ml-2 justify-center gap-4 m-2`}>
@@ -33,6 +35,16 @@ export default function SocialLinks() {
             style={{ transition: "all 0.5s" }}
           >
             <i className="fa-solid fa-calendar-days"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href={CV}
+            target="_BLANK"
+            style={{ transition: "all 0.5s" }}
+            className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-150`}
+          >
+            <i className="fa-solid fa-file-pdf"></i>
           </a>
         </li>
       </ul>
