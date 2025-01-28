aed3fe83c633d29d5486ce31597cb34d76f94e37
tomikorzu <tomykorzu@icloud.com>
2025-01-27 22:46:06 -0300

    feat(welcomeData / SocialLinks): make it more usefull

--- src/shared/components/SocialLinks/index.tsx
+++ src/shared/components/SocialLinks/index.tsx
@@ -2,51 +2,25 @@ import data from "../../data/welcomeData.json";
 
 export default function SocialLinks() {
   const {
-    socials: { github, linkedin, calendly, CV },
+    socials: { links },
   } = data;
   return (
     <>
       <ul className={`flex items-center sm:ml-2 justify-center gap-4 m-2`}>
-        <li>
-          <a
-            className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-150`}
-            href={github}
-            target="_BLANK"
-            style={{ transition: "all 0.4s" }}
-          >
-            <i className="fa-brands fa-github"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-150`}
-            href={linkedin}
-            target="_BLANK"
-            style={{ transition: "all 0.5s" }}
-          >
-            <i className="fa-brands fa-linkedin"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-150`}
-            href={calendly}
-            target="_BLANK"
-            style={{ transition: "all 0.5s" }}
-          >
-            <i className="fa-solid fa-calendar-days"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href={CV}
-            target="_BLANK"
-            style={{ transition: "all 0.5s" }}
-            className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-150`}
-          >
-            <i className="fa-solid fa-file-pdf"></i>
-          </a>
-        </li>
+        {links.map((link) => {
+          return (
+            <li>
+              <a
+                className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-150`}
+                href={link.url}
+                target="_BLANK"
+                style={{ transition: "all 0.4s" }}
+              >
+                <i className={link.icon}></i>
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
