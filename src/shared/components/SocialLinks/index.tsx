46920e615375f681f8ef88e5f2d555547c1dc1b5
tomikorzu <tomykorzu@icloud.com>
2025-01-29 02:20:14 -0300

    fix(SocialLinks): change the hover brightness

--- src/shared/components/SocialLinks/index.tsx
+++ src/shared/components/SocialLinks/index.tsx
@@ -11,7 +11,7 @@ export default function SocialLinks() {
           return (
             <li>
               <a
-                className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-150`}
+                className={`text-lg bg-[#3A506B] rounded-full flex justify-center items-center w-10 h-10  hover:brightness-125`}
                 href={link.url}
                 target="_BLANK"
                 style={{ transition: "all 0.4s" }}
